60b0d087f40ce53e9b007639e78f971f3d7cc7b3
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-11-22 15:46:27 +0200

    deployingToHeruko

---
+++ server.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express= require('express');
+require('dotenv').config();
+const cors= require('cors');
+
+const server =express();
+server.use(cors());
+
+const PORT=process.env.PORT|| 3000;
+let weather =require('./data/weather.json');
+server.get('/',(req,res)=>{
+    res.send(weather);
+})
+function Weather(){
+    
+
+
+}
+
+
+
+
+server.listen(PORT,()=>{
+    console.log("Everything is good");
+})
