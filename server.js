c2f6039e9c6ab9c5397555941e2eea9fb7fd71aa
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-11-25 10:20:40 +0200

    test

--- server.js
+++ server.js
@@ -21,77 +21,56 @@ server.get('*', allRoutes);
 server.use(errorHandler);
 
 
-// server.get('/test',testLocationHandler);
-// function testLocationHandler(req,res){
-//     let cityName=res.query.city;
-//     // let form=res.query.form;
-//     // let lat=res.query.lat;
-//     // let lon=res.query.lon;
-//     // INSERT INTO locations (search_quer,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4)`
-//     let SQL= `SELECT * FORM locations WHERE search_quer=${cityName};`// query
-//     client.query(SQL) //to link it to database
-//     .then((result)=>{ 
-//         res.send(result.rows);
-//     })
-// }
-
-
-
 function locationHandlerFunc(req, res) {
     let cityName = req.query.city;
     let selectuery = `SELECT * FROM locations WHERE search_query='${cityName}';`;
     client.query(selectuery)
-    .then(result => {
-        // console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'+result.rowCount)
-        // return result.rows;  
-           if(result.rowCount){
-            console.log(result.rows);
-            res.json(result.rows[0]);
-    }
-        else{
-            console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
-            getCityLocation(cityName,req,res)
-            .then(locaData => { //return data from location IQ
-                res.status(200).json(locaData);
-            })
-            .catch(() => {
-                errorHandler('Something went wrong', req, res);
-            });
-        }
-    })
-    .catch(() => {
-        errorHandler('Something went wrong', req, res);
-    });
-   
+        .then(result => {
+            // console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii'+result.rowCount)
+            // return result.rows;  
+            if (result.rowCount) {
+                console.log(result.rows);
+                res.json(result.rows[0]);
+            }
+            else {
+                console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+                getCityLocation(cityName)
+                    .then(locaData => { //return data from location IQ
+                        console.log('**********************************************************************************');
+                        console.log(locaData);
+                        res.status(200).json(locaData);
+                    })
+
+            }
+        })
+
 }
 
 
 
 
- 
-function getCityLocation(cityName,req,res) {
+
+function getCityLocation(cityName) {
     let GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
     let url = `https://eu1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`;
-        return superagent.get(url)//bring me the data from url
-            .then(myData => {
-                  // save to the DB
-                 let formatted_query = myData.body[0].display_name;
-                 let longitude = myData.body[0].lon;
-                 let latitude = myData.body[0].lat;
-                 let insertLocation = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4)`;
-                let safeValues = [cityName,formatted_query, latitude, longitude];
-                client.query(insertLocation, safeValues)
-                .then(()=>{
-                    res.send('your data has been added successfully!!');
+    return superagent.get(url)//bring me the data from url
+        .then(myData => {
+            // save to the DB
+            let formatted_query = myData.body[0].display_name;
+            let longitude = myData.body[0].lon;
+            let latitude = myData.body[0].lat;
+            let insertLocation = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4)`;
+            let safeValues = [cityName, formatted_query, latitude, longitude];
+           return client.query(insertLocation, safeValues)
+                .then(() => {
+                    // res.send('your data has been added successfully!!');
+                    console.log(myData.body)
+                    let locObj = new Location(cityName, myData.body);//------ just the impotrtant data-------
+                    return locObj//to the client
                 })
-                console.log(myData.body)
-                let locObj = new Location(cityName, myData.body);//------ just the impotrtant data-------
-                return locObj//to the client
-            })
-            .catch(() => {
-                errorHandler('Something went wrong',req,res);
-            });
-    
+        })
+
+
 }
 
 
